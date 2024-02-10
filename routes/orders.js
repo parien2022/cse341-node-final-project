41c5d17e42eb1e2477b7b5337c03a8ed0a3ef80a
Alitares27 <aldair2795@hotmail.com>
2024-02-10 16:32:00 -0300

    fixing bugs

--- routes/orders.js
+++ routes/orders.js
@@ -1,13 +1,13 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const validation = require('../middleware/validate')
+const ordersController = require('../controllers/orders');
+//const { isAuthenticated } = require('../middleware/authenticate');
 
-const ordersController = require("../controllers/orders");
-const { isAuthenticated } = require("../middleware/authenticate");
-
-router.get("/", ordersController.getOrders);
-router.get("/:id", ordersController.getOrder);
-router.get("/status/:status", ordersController.getOrderByStatus)
-router.post("/", isAuthenticated, ordersController.createOrder);
-router.put("/:id", isAuthenticated, ordersController.updateOrder);
-router.delete("/:id", isAuthenticated, ordersController.deleteOrder);
+router.get('/', ordersController.getOrders);
+router.get('/:id', ordersController.getOrder);
+router.get('/name/:conditions', ordersController.getOrderByStatus)
+router.post('/', validation.validateOrders, ordersController.createOrder);
+router.put('/:id', validation.validateOrders, ordersController.updateOrder);
+router.delete('/:id', ordersController.deleteOrder);
 
 module.exports = router;
