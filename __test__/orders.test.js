d8c0874032391b32b4d5a694bc5c2b1c8207a001
Alitares27 <aldair2795@hotmail.com>
2024-02-20 02:18:26 -0300

    start testing orders and users

--- __test__/orders.test.js
+++ __test__/orders.test.js
@@ -2,12 +2,74 @@ const request = require('supertest')
 const app = require('../app')
 
 /*
- * Testing Get all Orders
+ * Testing Orders endpoints
  */
-it('responds with json containing all the orders', (done) => {
-  request(app)
-    .get('/orders')
-    .set('Accept', 'application/json')
-    .expect('content-type', /json/)
-    .expect(200,done());
+describe('Testing Orders', () => {
+  it('Responds with JSON with all the orders', (done) => {
+    request(app)
+      .get('/orders')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200, done());
+  })
+
+  it('Responds with JSON with an order by ID', (done) => {
+    request(app)
+      .get('/orders/:id')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200, done());
+  })
+
+  it('Responds with JSON with an order by Status', (done) => {
+    request(app)
+      .get('/orders/name/:conditions')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200, done());
+  })
+
+  it('Responds with JSON with the order saved', (done) => {
+    const data = {
+      user: 'messiL',
+      date: '02/14/2024',
+      conditions: 'cancelled',
+    };
+    request(app)
+      .post('/orders')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(201)
+      .end(err => {
+        if (err) return done(err);
+        done();
+      })
+  })
+
+
+  
+
+})
+
+/*
+ * Testing Users endpoints
+ */
+describe('Testing Users', () => {
+  it('Responds with JSON with all the users', (done) => {
+    request(app)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200, done());
+  })
+
+  it('Responds with JSON with an User by ID', (done) => {
+    request(app)
+      .get('/users/:id')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200, done());
+  })
+
 })
